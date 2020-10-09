08f37c5e45ba85f905da280ffac5aa1f13005543
Jamie Johnstone <j@miej.co>
2020-10-09 17:23:32 +0100

    Change apig resource to analysis

--- terraform/modules/apigateway/main.tf
+++ terraform/modules/apigateway/main.tf
@@ -6,13 +6,13 @@ resource "aws_api_gateway_rest_api" "apig" {
 resource "aws_api_gateway_resource" "proxy" {
    rest_api_id = aws_api_gateway_rest_api.apig.id
    parent_id   = aws_api_gateway_rest_api.apig.root_resource_id
-   path_part   = "{proxy+}"
+   path_part   = "analysis"
 }
 
 resource "aws_api_gateway_method" "proxy" {
    rest_api_id   = aws_api_gateway_rest_api.apig.id
    resource_id   = aws_api_gateway_resource.proxy.id
-   http_method   = "ANY"
+   http_method   = "POST"
    authorization = "NONE"
 }
 
@@ -22,7 +22,7 @@ resource "aws_api_gateway_integration" "lambda" {
    http_method = aws_api_gateway_method.proxy.http_method
 
    integration_http_method = "POST"
-   type                    = "AWS_PROXY"
+   type                    = "AWS"
    uri                     = var.lambda_arn
 }
 
