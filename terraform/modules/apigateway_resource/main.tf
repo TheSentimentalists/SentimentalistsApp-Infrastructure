0381ef2b6d26a7c48d81ad77db685b7ce1dc3674
Jamie Johnstone <j@miej.co>
2020-10-10 18:32:56 +0100

    Update apig_res module naming convention

--- terraform/modules/apigateway_resource/main.tf
+++ terraform/modules/apigateway_resource/main.tf
@@ -1,47 +1,47 @@
-resource "aws_api_gateway_resource" "proxy" {
+resource "aws_api_gateway_resource" "apig_resource" {
    rest_api_id = var.apig_id
    parent_id   = var.apig_root_id
    path_part   = "analysis"
 }
 
-resource "aws_api_gateway_method" "proxy" {
+resource "aws_api_gateway_method" "apig_resource" {
    rest_api_id = var.apig_id
-   resource_id   = aws_api_gateway_resource.proxy.id
+   resource_id   = aws_api_gateway_resource.apig_resource.id
    http_method   = "POST"
    authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "lambda" {
+resource "aws_api_gateway_integration" "apig_resource" {
    rest_api_id = var.apig_id
-   resource_id = aws_api_gateway_method.proxy.resource_id
-   http_method = aws_api_gateway_method.proxy.http_method
+   resource_id = aws_api_gateway_method.apig_resource.resource_id
+   http_method = aws_api_gateway_method.apig_resource.http_method
 
    integration_http_method = "POST"
    type                    = "AWS"
    uri                     = var.lambda_arn
 }
 
-resource "aws_api_gateway_method_response" "proxy" {
+resource "aws_api_gateway_method_response" "apig_resource" {
     rest_api_id = var.apig_id
-    resource_id = aws_api_gateway_method.proxy.resource_id
-    http_method = aws_api_gateway_method.proxy.http_method
+    resource_id = aws_api_gateway_method.apig_resource.resource_id
+    http_method = aws_api_gateway_method.apig_resource.http_method
     status_code = "200"
     response_parameters = { 
         "method.response.header.Access-Control-Allow-Origin" = true 
     }
 }
 
-resource "aws_api_gateway_integration_response" "proxy" {
+resource "aws_api_gateway_integration_response" "apig_resource" {
     rest_api_id = var.apig_id
-    resource_id = aws_api_gateway_method.proxy.resource_id
-    http_method = aws_api_gateway_method.proxy.http_method
-    status_code = aws_api_gateway_method_response.proxy.status_code
+    resource_id = aws_api_gateway_method.apig_resource.resource_id
+    http_method = aws_api_gateway_method.apig_resource.http_method
+    status_code = aws_api_gateway_method_response.apig_resource.status_code
     response_parameters = { 
         "method.response.header.Access-Control-Allow-Origin" = "'*'" 
     }
 }
 
-resource "aws_lambda_permission" "apigw" {
+resource "aws_lambda_permission" "apig_resource" {
    statement_id  = "AllowAPIGatewayInvoke"
    action        = "lambda:InvokeFunction"
    function_name = var.lambda_function_name
