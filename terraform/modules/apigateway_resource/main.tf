108c5a77a66bcb8c113ecbd6bdfbcd171d19ef6c
Jamie Johnstone <j@miej.co>
2020-10-10 18:19:03 +0100

    Hardcode deployment stage for testing

--- terraform/modules/apigateway_resource/main.tf
+++ terraform/modules/apigateway_resource/main.tf
@@ -47,7 +47,7 @@ resource "aws_api_gateway_deployment" "apig" {
    ]
 
    rest_api_id = var.apig_id
-   stage_name  = var.apig_stage
+   stage_name  = "prod"
 }
 
 resource "aws_lambda_permission" "apigw" {
